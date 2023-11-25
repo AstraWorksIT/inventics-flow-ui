a9771a76669de461bb2935ac6cb32d17c62e17dc
Nestor Abiangang <nestorabiawuh@gmail.com>
2023-11-26 02:21:17 +0300

    Initial commit

---
+++ src/main/java/com/astraworks/inventicsflowui/InventicsFlowUiApplication.java
@@ -0,0 +1,13 @@
+package com.astraworks.inventicsflowui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventicsFlowUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventicsFlowUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/astraworks/inventicsflowui/InventicsFlowUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astraworks.inventicsflowui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventicsFlowUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
